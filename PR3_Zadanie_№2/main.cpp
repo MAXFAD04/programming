2684b61fb1300e2daefb55cc6390448ba4cfd2e6
MAXFAD04 <125014855+MAXFAD04@users.noreply.github.com>
2023-03-12 19:12:30 +0300

    Update main.cpp

--- PR3_Zadanie_№2/main.cpp
+++ PR3_Zadanie_№2/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include "rational.h"
 
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+
+	int size;
+	cout << "������� ���������� ������ -> "; cin >> size;
+	cout << "\n";
+	rational* arr = new rational[size];
+	int a, b;
+
+	cout << "������� ����� ����� ������ ����� ������\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "������� ��������� � ����������� ����� ����� " << i + 1 << " : "; cin >> a >> b;
+		(arr[i]).set(a,b);
+	}
+	cout << "\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\n����� ����� " << i + 1 << " : ";
+		(arr[i]).show();
+	}
+	cout << "\n";
+
+	delete[] arr;
+
+	return 0;
+}
